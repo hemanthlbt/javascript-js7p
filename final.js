18da32b35783833f3c9e867952ef3cc85e203a32
Hemanth kumar lingabathina <buntytwo@proton.me>
2024-10-25 14:32:49 +0530

    Add files via upload

--- final.js
+++ final.js
@@ -1,4 +1,43 @@
-console.log("java script completion day")
-console.log("i will put an end to this today!")
-console.log("lets make it 200 contributions today!")
+const newarr = ["apple","banana",1,2,3,null,undefined]
+//console.log(newarr)
+
+const arrt =["samantha","tammana","kajal","niggabad"]
+
+
+const another = [...newarr,...arrt]
+// console.log(another)
+
+// console.log(another[0])
+
+const hemanth = [1,2,3,4,5,6,7,8,9,10]
+// console.log(hemanth.push)
+
+hemanth.unshift(21)
+
+// console.log(hemanth)
+
+hemanth.push(19)
+
+// console.log(hemanth)
+
+hemanth.pop()
+
+// console.log(hemanth)
+
+hemanth.shift()
+
+// console.log(hemanth)
+
+// console.log(hemanth.lenght())
+
+var items = [1,2,3,4,5,6,7,8,9]
+
+console.log(items.lastIndexOf(2))
+
+
+
+
+
+
+
 
