e19e7105c51455a0b5c7ce046aa3b161379a010d
Hemanth kumar lingabathina <buntytwo@proton.me>
2024-10-26 16:52:41 +0530

    failed attempt

---
+++ oct.js
@@ -0,0 +1,24 @@
+const items = {
+
+item1: "milk",
+item2: "biriyani",
+item3: "protein",
+item4: "breakfast",
+item5: "cococola",
+
+
+}
+
+
+function total() {
+
+return items+1;
+
+
+}
+
+
+var alltotal = (items+100)
+console.log(alltotal)
+
+
