b955b6072cd3e8789f938873790f39f03f54caf8
Hemanth kumar lingabathina <buntytwo@proton.me>
2024-11-02 17:44:37 +0530

    Add files via upload

--- interview.js
+++ interview.js
@@ -27,4 +27,105 @@
 
 // object constructor way.
 
-var object = object();
+// var object = object();
+
+//object's create method.
+
+
+
+
+
+
+// arrays.
+
+var newarray = ["apples","oranges","bananas","oats","mulesi","eggs"]
+
+// console.log(newarray[3])
+
+//newarray.push(100)
+
+
+//combining two or more arrays as one array.
+
+
+// var grapes = ["apple","samsumg","nikion","staples","tomatoes"]
+// var bananas = ["calofornia","newyork","tampabay","southpark","spark"]
+
+// console.log(grapes.concat(bananas))
+
+// Using spread operator.
+
+// console.log(...grapes,...bananas)
+
+//copy clone an array.
+
+//  normal copy ,shallow copy , deep copy.
+
+
+// const arr = [1,2,3,4,5]
+
+// const copy = arr.slice();
+// // console.log(arr)
+// console.log(copy)
+
+
+// var original  = [true,true,false,undefined,null]
+
+// var copy1 = original.slice(0);
+// // console.log(copy1)
+
+// // copying using spread operator.
+// var copy2 = [...original]
+// console.log(copy1,copy2)
+
+// // using deep copy.
+// var deeparray = [["freecodecamp"]]
+
+// var items = ["cars","bikes","computeres"]
+
+// console.log(items[0].push("this is great");
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
