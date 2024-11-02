c46fc89cb72b346aeec6964a094aa52f93cd9e69
Hemanth kumar lingabathina <buntytwo@proton.me>
2024-11-02 15:27:18 +0530

    Add files via upload

---
+++ interview.js
@@ -0,0 +1,30 @@
+// var object = {
+
+// name : "hemanthkumar",
+// age : 25
+
+// }
+
+// console.log(object)
+
+// //it can be of anytype.
+
+
+
+// var newone = {
+
+// names : ["hemanth","kumar","lingabathina"],
+// places : ["hyderabad","nellore","kerala","hyderabad"]
+
+
+// }
+
+
+// console.log(newone)
+
+
+// var bunty = Object();
+
+// object constructor way.
+
+var object = object();
