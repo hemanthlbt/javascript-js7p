21a3d2582bbec745c10dcdc97f831754509f952a
Hemanth kumar lingabathina <buntytwo@proton.me>
2024-11-06 00:11:59 +0530

    Add files via upload

---
+++ trickparse.js
@@ -0,0 +1,6 @@
+let names = ["samara","esha","trisha","hemanth","lingabathina"]
+
+console.log(JSON.stringify(names))
+console.log(JSON.parse(JSON.stringify(names)))
+
+// ahh it became a nested one now.
