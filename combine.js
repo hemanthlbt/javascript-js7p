63e817a3a70267999046c6a1509f6f3eef2816e1
Hemanth kumar lingabathina <buntytwo@proton.me>
2024-11-05 23:51:21 +0530

    Add files via upload

---
+++ combine.js
@@ -0,0 +1,18 @@
+//combining 2 or more arrays.
+
+//using concat method => Es6.
+let array1 = ["apples","mangoes","bananas","grapes","aprocots"];
+let array2 = ["burgers","pizzas","sodas","pastas","hakkanoodles"];
+let array3 = ["lays","bingo","kurkure","hippos","bytes"];
+// let array4 = array1.concat(array2,array3)
+// console.log(array4)
+
+
+//using spread operator.
+// let array4 = [...array1,...array2,...array3]
+// console.log(array4)
+
+
+
+
+
