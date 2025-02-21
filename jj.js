2cb9b3d3c172b1bb14bad1ec5f02ded188c9ba89
superhuman69 <buntytwo@proton.me>
2025-02-21 16:28:54 +0530

    firstjson

---
+++ jj.js
@@ -0,0 +1,23 @@
+	let ccc  =  `[
+	{
+		"name" : "big company",
+		"numberofemployees" : 1000,
+		"ceo": "hemanth kumar lingabathina",
+		"rating" : 20,
+	},
+	{
+		"name" : "google",
+		"numberofemployees" : 2000,
+		"ceo" : "sundarpichai",
+		"rating" : 0,
+	},
+
+	{
+		"name" : "apple",
+		"numberofemployees" : 3000,
+		"ceo" : "timcook",
+		"rating": 10, 
+	}
+]`
+
+console.log(ccc)
