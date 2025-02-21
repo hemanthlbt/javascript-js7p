3478e651b90f9fda3eba07deb908275e23ec3d7a
superhuman69 <buntytwo@proton.me>
2025-02-21 09:30:16 +0530

    errors

---
+++ errors.js
@@ -0,0 +1,32 @@
+//types of errors in javascript
+
+
+//reference error in javascript 
+
+// var a = 100
+// var b = 200
+// console.log(c)
+
+// type error
+
+// var x = 100
+// x.push(100)
+
+
+
+// systex error
+
+let obj1 = {
+	name : "Hemanthkumarlingabathina",
+	age : 25,
+	place : "nellore"
+	birthplace : "hyderabad"
+
+}
+
+
+
+console.log(obj1)
+
+
+
