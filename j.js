557bcfb62645499b78a60a1313776ed0fec6dd71
superhuman69 <buntytwo@proton.me>
2025-02-21 16:40:35 +0530

    sa

---
+++ j.js
@@ -0,0 +1,26 @@
+var bunty = [
+	{
+		"name" : "google",
+		"founder" : "Sundarpichai",
+		"ceo" : "Sundar pichai",
+		"location" :"siliconvalley",
+	},
+
+	{
+		"name" : "apple",
+		"founder" : "Stevejobs",
+		"ceo" : "timcook",
+		"location" : "appletechpark",
+	},
+
+	{
+		"name" : "spacex",
+		"founder" : "elonmusk",
+		"ceo" : "elonmusk",
+		"location": "siliconvalley",
+	}
+]
+
+
+console.log(JSON.stringify(bunty))
+
