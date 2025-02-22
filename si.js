8b335bbf5b9143ab76899794061c3062263e08c5
superhuman69 <buntytwo@proton.me>
2025-02-22 17:59:10 +0530

    leveup

---
+++ si.js
@@ -0,0 +1,17 @@
+//this is a set timer function..
+
+
+setTimeout(function(){console.log("This will appear after 5 seconds ")},5000)
+
+
+// now lets see what is a setInterval is...
+
+
+// setInterval(function(){"i dont know that this will dooo..."},5000)
+
+
+function greet() {
+	console.log("Hi hemanth kuamr lingabathian")
+}
+
+setInterval(greet,1000)
