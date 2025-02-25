057ca4ad8cd86c97c20534e070ba1c44fc8aab5d
superhuman69 <buntytwo@proton.me>
2025-02-25 15:14:10 +0530

    errorhandling

---
+++ err.js
@@ -0,0 +1,15 @@
+//how to handle errors in java script
+
+try {
+
+
+
+	console.log("this is jva script")
+	console.log("this is traial and error")
+	consoole.log("hemanth kumar lingabaathiana")
+}
+
+
+catch(error){
+	
+}
