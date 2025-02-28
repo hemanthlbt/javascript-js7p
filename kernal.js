0ab6ff2163f1c9a488abcc88860bd52e50077e8e
hemanthlbt <lingabathina@icloud.com>
2025-02-28 16:45:40 +0530

    kernaldeve

---
+++ kernal.js
@@ -0,0 +1,17 @@
+let kernal = 100;
+let bunty =  100;
+
+
+
+
+if (kernal = bunty) {
+
+	console.log("I need to complete java script and linux this year")
+}
+
+
+else {
+	console.log("you are such a disgrace to the community hemanthh kuamr")
+} 
+
+
