afb5e84db6b429e8ca4bda67cfe15b36fb67ffaf
superhuman69 <buntytwo@proton.me>
2025-02-28 10:05:40 +0530

    sa

---
+++ tj.js
@@ -0,0 +1,10 @@
+let jsondata = `[
+	{"name":"hemanthkumar","age":"100","place":"hyderabad,India"},
+{"name":"akhilraghavendra","age":"200","place":"Banglore,India"}
+]`
+
+
+console.log(JSON.parse(jsondata))
+console.log(JSON.stringify(jsondata))
+
+
