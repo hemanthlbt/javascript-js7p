8616cc919fcd4d7bb6b990be5a96a4af9eb46f0c
Hemanth kumar lingabathina <buntytwo@proton.me>
2025-06-02 13:16:44 +0530

    Add files via upload

---
+++ megan.js
@@ -0,0 +1,36 @@
+hello();
+greet(hemanth);
+
+
+
+function hello() {
+	console.log("this is hemanth kumar") 
+}
+
+function greet(callback) {
+	console.log("this is hyderabad")
+	callback();
+}
+
+
+
+function leave() {
+	console.log("leave!")
+}
+
+
+function hemanth() {
+	console.log("console this after me")
+}
+
+
+
+
+
+
+
+
+
+
+
+
