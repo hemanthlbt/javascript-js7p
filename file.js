ad6cc563c15efc5d1a4a011b7ea1517d227d515b
hemanthlbt <lingabathina@icloud.com>
2025-06-08 00:39:41 +0530

    next

---
+++ file.js
@@ -0,0 +1,7 @@
+let car1 = "honda"
+let car2 = "audi"
+let car3 = "lamborgini"
+
+console.log(car1+car2+car3)
+console.log("now that iv'e removed this i need to practice this again and check this if this shit os working or not")
+
