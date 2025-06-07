afce2b2e097b97d86b8c08fdc4023ba6f03acb41
hemanthlbt <lingabathina@icloud.com>
2025-06-08 00:46:04 +0530

    all

--- file.js
+++ file.js
@@ -4,4 +4,5 @@ let car3 = "lamborgini"
 
 console.log(car1+car2+car3)
 console.log("now that iv'e removed this i need to practice this again and check this if this shit os working or not")
+console.log("hemanth kumar Lingabathina")
 
