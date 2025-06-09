bcab0f2aae2f6ee7e015861bca2638b3e9dcd96e
Hemanth kumar lingabathina <buntytwo@proton.me>
2025-06-09 19:26:01 +0530

    Add files via upload

---
+++ obj.js
@@ -0,0 +1,34 @@
+const family  = {
+
+name1 :  "hemanthkumarlingabathina",
+name2 : "haribabu lingabtihna",
+name3 : "sreevani devarapalli",
+name4 : "sravya lingabathina",
+ages : [hemanth = "26", sravya = "27", haribabu = "61", sreevani = "54"],
+name5 : {
+	c1 : "hyderabd",
+	c2 : "delhi",
+	c3 : "mumbai",
+	c4:"banglore",
+	c5 : "jaipur",
+	c6 : "ahmedabad",
+	c7 : "lucknnow",
+	c8 : "chennai",
+	c9 : "kochi",
+	c10: "surat",
+	c11: "vishakapatnam",
+	c12 : "gawhati",
+	c13 : "sikkim"
+}
+}
+
+
+JSON.stringify(family)
+console.log(family)
+
+console.log(typeof(family))
+
+
+
+
+
