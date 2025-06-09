a0a70a5fe0207f9bc38cdd041a644a5ca5b0a481
Hemanth kumar lingabathina <buntytwo@proton.me>
2025-06-09 17:40:22 +0530

    Add files via upload

--- last.js
+++ last.js
@@ -1,1 +1,26 @@
-console.log("this is a warning man complete it now ir ready fro some Distruction")
+(()=>{console.log("hemanthkumarlingabthina")})  ();
+
+ 
+function hemanth(a,c,b) {
+	return a+b*c
+}
+console.log(hemanth(1,2,45))
+
+
+let bunty = function() {
+	console.log("this is a function expression")
+}
+
+bunty()
+
+
+//settimeout function
+
+setTimeout(function(){console.log("this is a timer function")},4000);
+setTimeout(()=>{console.log("this is a arrow function with a timer")},6000);
+
+
+
+
+
+
