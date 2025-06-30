9772b96ea1dfc252fd4b709c184f22c253709332
hemanthlbt <lingabathina@icloud.com>
2025-06-30 20:04:45 +0530

    eval

---
+++ ofty.js
@@ -0,0 +1,36 @@
+let hemanth = "hemanthkumar"
+let q = null;
+let w = 5.6;
+// console.log(typeof(hemanth))
+// console.log(typeof(w))
+// console.log(typeof(q))
+
+
+var obj1 = {
+    name : "audi",
+    name1 : "pascal",
+
+}
+
+// console.log(typeof(obj1))
+let bbb = (2,3,4)
+
+// console.log(typeof(bbb))
+
+
+var user = "string";
+var user2 = 34;
+// console.log(typeof user2)
+console.log(eval("2+2+34+56+78"))
+
+
+
+
+
+
+
+
+
+
+
+
